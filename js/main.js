c8a975c9384faafca0bd87457d1f72b915acc7e8
kyle <nojaim.k@gmail.com>
2024-06-15 10:33:15 -0400

    equals function needs to remove needless ()

--- js/main.js
+++ js/main.js
@@ -10,6 +10,16 @@ function operation(event){
     }else if(event.target.classList.contains('parOpen')){
         equation.push('(')
         display = equation.join('')
+    }else if(event.target.classList.contains('equals')){
+        let value = equation.map((el,i) => {
+            if (i === 0){
+                el = '(' + el
+            }else if(isNaN(el)){
+                el = ')' + el + '('
+            }else if (!isNaN(el)){
+                el = el
+            }
+        })
     }
     document.querySelector('.equation').innerText = display
 }
