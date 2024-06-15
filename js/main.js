07da09b608482a28252a7b32dc53a9adb797dad5
kyle <nojaim.k@gmail.com>
2024-06-15 00:02:40 -0400

    clear + paropen (blueprint), needs = function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 let buttons = document.querySelectorAll('button')
 
+let equation = []
+
+let display = ''
+
+function operation(event){
+    if (event.target.classList.contains('clear')){
+        equation = []
+    }else if(event.target.classList.contains('parOpen')){
+        equation.push('(')
+        display = equation.join('')
+    }
+    document.querySelector('.equation').innerText = display
+}
+
 buttons.forEach(button => {
     button.addEventListener('click', operation)
 })
