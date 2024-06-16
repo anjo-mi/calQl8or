3eb5101940d946f8fbd36fd104810900cffb340b
kyle <nojaim.k@gmail.com>
2024-06-16 14:12:12 -0400

    = needs functionality, figure out how to add spec syms w js

--- js/main.js
+++ js/main.js
@@ -7,22 +7,79 @@ let display = ''
 function operation(event){
     if (event.target.classList.contains('clear')){
         equation = []
+        display = equation.toString()
     }else if(event.target.classList.contains('parOpen')){
         equation.push('(')
         display = equation.join('')
+    }else if(event.target.classList.contains('parClose')){
+        equation.push(')')
+        display = equation.join('')
+    }else if(event.target.classList.contains('percent')){
+        equation.push('%')
+        display = equation.join('')
+    }else if(event.target.classList.contains('root')){
+        equation.push('&radic;')
+        display = equation.join('')
+    }else if(event.target.classList.contains('one')){
+        equation.push('1')
+        display = equation.join('')
+    }else if(event.target.classList.contains('two')){
+        equation.push('2')
+        display = equation.join('')
+    }else if(event.target.classList.contains('three')){
+        equation.push('3')
+        display = equation.join('')
+    }else if(event.target.classList.contains('divide')){
+        equation.push('&div;')
+        display = equation.join('')
+    }else if(event.target.classList.contains('power')){
+        equation.push('^')
+        display = equation.join('')
+    }else if(event.target.classList.contains('four')){
+        equation.push('4')
+        display = equation.join('')
+    }else if(event.target.classList.contains('five')){
+        equation.push('5')
+        display = equation.join('')
+    }else if(event.target.classList.contains('six')){
+        equation.push('6')
+        display = equation.join('')
+    }else if(event.target.classList.contains('times')){
+        equation.push('x')
+        display = equation.join('')
+    }else if(event.target.classList.contains('factorial')){
+        equation.push('!')
+        display = equation.join('')
+    }else if(event.target.classList.contains('seven')){
+        equation.push('7')
+        display = equation.join('')
+    }else if(event.target.classList.contains('eight')){
+        equation.push('8')
+        display = equation.join('')
+    }else if(event.target.classList.contains('nine')){
+        equation.push('9')
+        display = equation.join('')
+    }else if(event.target.classList.contains('minus')){
+        equation.push('-')
+        display = equation.join('')
+    }else if(event.target.classList.contains('pi')){
+        equation.push('&pi;')
+        display = equation.join('')
+    }else if(event.target.classList.contains('decimal')){
+        equation.push('.')
+        display = equation.join('')
+    }else if(event.target.classList.contains('zero')){
+        equation.push('0')
+        display = equation.join('')
+    }else if(event.target.classList.contains('plus')){
+        equation.push('+')
+        display = equation.join('')
+    }else if(event.target.classList.contains('backspace')){
+        equation.pop()
+        display = equation.join('')
     }else if(event.target.classList.contains('equals')){
-        let value = equation.map((el,i) => {
-            if (i === 0){
-                el = '(' + el
-            }else if(isNaN(el)){
-                el = ')' + el + '('
-            }else if (!isNaN(el)){
-                el = el
-            }
-            let placeHolder = value.toString()
-            display = placeHolder.replace('()', '')
-            document.querySelector('.answer').innerText = parseInt(display, 10)
-        })
+        
+        display = equation.join('')
     }
     document.querySelector('.equation').innerText = display
 }
